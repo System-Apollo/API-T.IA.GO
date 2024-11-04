abff489c6490cf6856b0ba20c66103a82c402b1b
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-11-04 09:30:33 -0300

    FIX: adjusting schema to accept uuid str

--- src/database/schema.py
+++ src/database/schema.py
@@ -1,7 +1,7 @@
 from marshmallow import Schema, fields
 
 class UserSchema(Schema):
-    id = fields.Integer(dump_only=True)
+    id = fields.String(dump_only=True)
     username = fields.String(dump_only=True)
     email = fields.String(dump_only=True)
     password = fields.String(dump_only=True)
--- src/models/token_blocklist.py
+++ src/models/token_blocklist.py
@@ -1,13 +1,9 @@
 from src.utils.config.extensions import db
-from uuid import uuid4
 from datetime import datetime, timezone
-
-def generate_uuid():
-    return str(uuid4())
 
 class TokenBlocklist(db.Model):
     __tablename__ = 'token_blocklist'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String, primary_key=True)
     jti = db.Column(db.String, unique=True, nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
 
