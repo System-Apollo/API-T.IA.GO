587990e0ff215a10382a35250f3cb8b522f10cec
Matheus-Fontinele-Alves-Vieria <matheusfont.123@gmail.com>
2024-11-11 09:12:33 -0300

    feat: adding new user attributes

--- src/database/schema.py
+++ src/database/schema.py
@@ -2,7 +2,10 @@ from marshmallow import Schema, fields
 
 class UserSchema(Schema):
     id = fields.String(dump_only=True)
+    name = fields.String(dump_only=True)
+    last_name = fields.String(dump_only=True)
     username = fields.String(dump_only=True)
+    company = fields.String(dump_only=True)
     email = fields.String(dump_only=True)
     password = fields.String(dump_only=True)
     cpf_cnpj = fields.String(dump_only=True)
