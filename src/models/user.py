0dabd08d9bb8c2b6db5ea4e15318f74cecc79eef
Matheus-Fontinele-Alves-Vieria <matheusfont.123@gmail.com>
2024-11-11 09:13:53 -0300

    feat: adding new user attributes

--- src/models/user.py
+++ src/models/user.py
@@ -8,20 +8,26 @@ def generate_uuid():
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.String, primary_key=True, default=generate_uuid)
+    name = db.Column(db.String(64), index=True, unique=True)
+    last_name = db.Column(db.String(64), index=True, unique=True)
     username = db.Column(db.String(64), index=True, unique=True)
+    company = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password = db.Column(db.String(512))
     cpf_cnpj = db.Column(db.String(14), unique=True)
 
-    def __init__(self, username, email, password, cpf_cnpj):
+    def __init__(self, name, last_name, company, email, password, cpf_cnpj):
         self.id = generate_uuid()
-        self.username = username
+        self.name = name
+        self.last_name = last_name
+        self.username = f"{name} {last_name}"
+        self.company = company
         self.email = email
         self.password = generate_password_hash(password)
         self.cpf_cnpj = cpf_cnpj
 
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f'<User {self.name} {self.last_name}>'
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -32,6 +38,10 @@ class User(db.Model):
     @classmethod
     def get_user_by_username(cls, username):
         return cls.query.filter_by(username=username).first()
+
+    @classmethod
+    def get_email(cls, email):
+        return cls.query.filter_by(email=email).first()
 
     def save_to_db(self):
         db.session.add(self)
